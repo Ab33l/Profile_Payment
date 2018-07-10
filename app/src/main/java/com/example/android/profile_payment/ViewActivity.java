354c4ee7894c8c8c9f027ebecd8f3e62b55d7d39
Ab33l <32242176+Ab33l@users.noreply.github.com>
2018-07-10 09:07:24 +0300

    Update ViewActivity.java

--- app/src/main/java/com/example/android/profile_payment/ViewActivity.java
+++ app/src/main/java/com/example/android/profile_payment/ViewActivity.java
@@ -7,11 +7,15 @@ import android.view.View;
 import android.widget.Button;
 
 public class ViewActivity extends AppCompatActivity {
+    private TextView TextUsername, TextEmail, TextPhone;
     private Button buttonEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+        TextUsername = findViewById(R.id.TextUsername);
+        TextEmail = findViewById(R.id.TextEmail);
+        TextPhone = findViewById(R.id.TextPhone);
         findViewById(R.id.buttonEdit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
