a353561093cfc7cbb2ff9cb25c450d55e6d4e216
Ab33l <32242176+Ab33l@users.noreply.github.com>
2018-07-10 10:46:28 +0300

    Update DonorClient.java

--- app/src/main/java/com/example/android/profile_payment/DonorClient.java
+++ app/src/main/java/com/example/android/profile_payment/DonorClient.java
@@ -1,1 +1,18 @@
-
+package com.example.android.profile_payment;
+import java.util.List;
+import com.example.android.profile_payment.Donor;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+public interface DonorClient {
+   @GET ("/donor/{id}")
+  Call<Donor> donor(
+    @Path("id") int id
+  );
+@POST ("donor/update")
+  Call<Donor> update (@Body Donor donor);
+}
