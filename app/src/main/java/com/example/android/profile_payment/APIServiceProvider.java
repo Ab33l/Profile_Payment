98729775eda01515be99137aa5523ef66beeff69
Ab33l <32242176+Ab33l@users.noreply.github.com>
2018-07-10 09:33:32 +0300

    Update APIServiceProvider.java

--- app/src/main/java/com/example/android/profile_payment/APIServiceProvider.java
+++ app/src/main/java/com/example/android/profile_payment/APIServiceProvider.java
@@ -1,1 +1,27 @@
+package com.example.android.profile_payment;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
+public class APIServiceProvider {
+  private static final String BASE_URL = "https://fadhili.herokuapp.com";
+
+  private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+  private static Retrofit.Builder builder =
+    new Retrofit.Builder()
+      .baseUrl(BASE_URL)
+      .addConverterFactory(
+        GsonConverterFactory.create()
+      );
+
+  private static Retrofit retrofit = builder.client(
+    httpClient.build()
+  ).build();
+
+  public static <S> S createService(
+    Class<S> serviceClass
+  ) {
+    return retrofit.create(serviceClass);
+  }
+}
